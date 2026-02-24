3db47a2559e4a3bfbea1cc31e979561e99f650d7
EL-CHANGO-v4 <dj.chango.v4@gmail.com>
2026-02-24 15:48:46 -0300

    Update database connection with port number

--- db.php
+++ db.php
@@ -1,14 +1,9 @@
 <?php
-// CONEXIÓN INTERNA - CHANGO v4
 $host = "mysql.railway.internal"; 
 $user = "root"; 
 $pass = "rJwwdlNlDAZRnKiYJUyYUxsJMZOIlySV"; 
 $db   = "railway"; 
-
-$conn = new mysqli($host, $user, $pass, $db);
-
-if ($conn->connect_error) {
-    die("Error satelital: " . $conn->connect_error);
-}
+$conn = new mysqli($host, $user, $pass, $db, 3306);
+if ($conn->connect_error) { die("Error"); }
 $conn->set_charset("utf8mb4");
 ?>
